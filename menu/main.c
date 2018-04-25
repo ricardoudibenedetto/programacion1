56efeddeccbba39a9235be22a275c85dbebe1589
ricardoudibenedetto <36193860+ricardoudibenedetto@users.noreply.github.com>
2018-04-25 12:09:17 -0300

    menu actualizado
    sectores

--- menu/main.c
+++ menu/main.c
@@ -14,8 +14,16 @@ typedef struct
     float sueldo;
     eFecha fechaIngreso;
     int isEmpty;
+    int idSector;
 
 }eEmpleado;
+typedef struct
+{
+    int id;
+    char descripcion[51];
+    int isEmpty;
+}eSector;
+//prototipos
 int menu();
 void inicializarEmpleados(eEmpleado vec[], int tam);
 int buscarLibre(eEmpleado vec[],int tam);
@@ -25,11 +33,21 @@ void mostrarEmpleados(eEmpleado vec[],int tam);
 void mostrarEmpleado(eEmpleado vec);
 void bajaEmpleado(eEmpleado vec[],int tam);
 void modificarEmpleado(eEmpleado vec[], int tam);
+void hardCode(eSector vec[]);
+//main
 int main()
 {
  int salir=0;
  eEmpleado empleados[50];
  inicializarEmpleados(empleados,50);
+ eSector sectores[5];
+
+ hardCode(sectores);
+ for(int i=0 ;i<5;i++)
+ {
+     printf(" %d-%s-%d\n",sectores[i].id, sectores[i].descripcion, sectores[i].isEmpty);
+ }
+ system("pause");
     do
     {
 
@@ -52,7 +70,7 @@ int main()
                 break;
             case 4:
                 printf("\nlistar\n\n");
-                printf("nombre\t\tsexo\tsueldo\t\tfecha de ingreso\tlegajo\n");
+                printf("nombre\t\tsexo\tsueldo\t\t\tfecha de ingreso\tlegajo\n");
                 mostrarEmpleados(empleados,50);
                 system("pause");
                 break;
@@ -76,6 +94,9 @@ int main()
     while(salir!=1);
     return 0;
 }
+
+//cuerpo de funciones
+//funcion main
 int menu()
 {
     int opcion;
@@ -93,6 +114,7 @@ int menu()
 
     return opcion;
 }
+//funcion que inicializa empleados en 1
 inicializarEmpleados(eEmpleado vec[],int tam)
 {
     int i;
@@ -103,7 +125,7 @@ inicializarEmpleados(eEmpleado vec[],int tam)
         }
 }
 
-
+//busca un lugar libre en el array para guardar a los empleados maximo 50
 int buscarLibre(eEmpleado vec[], int tam)
 {
    int indice=-1;
@@ -118,6 +140,8 @@ int buscarLibre(eEmpleado vec[], int tam)
     }
      return indice;
 }
+
+//funcion para buscar un empleado por el tipo de legajo
 int buscarEmpleado(eEmpleado vec[], int tam, int legajo)
 {
     int indice=-1;
@@ -130,6 +154,8 @@ int buscarEmpleado(eEmpleado vec[], int tam, int legajo)
         }
          return indice;
 }
+
+//Alta de empleados, si el legajo no se encuentra se carga un nuevo empleado
 void altaEmpleado(eEmpleado vec[],int tam)
 {
     eEmpleado nuevoEmpleado;
@@ -167,6 +193,9 @@ void altaEmpleado(eEmpleado vec[],int tam)
             printf("fecha de ingreso: ");
             scanf("%d %d %d", &nuevoEmpleado.fechaIngreso.dia, &nuevoEmpleado.fechaIngreso.mes, &nuevoEmpleado.fechaIngreso.anio);
 
+            printf("\n1.- RRHH \n2.- sistemas \n3.- administracion \n4.- compras \n5.- deposito\nIngrese un sector: ");
+            scanf("%d",&nuevoEmpleado.idSector);
+
             nuevoEmpleado. legajo = legajo;
             nuevoEmpleado.isEmpty = 0;
 
@@ -189,7 +218,7 @@ void mostrarEmpleados(eEmpleado vec[],int tam)
 }
 void mostrarEmpleado(eEmpleado vec)
 {
-    printf("%s\t\t%c\t%.2f\t%d/%d/%d\t\t%d\n",vec.nombre,vec.sexo,vec.sueldo,vec.fechaIngreso.dia,vec.fechaIngreso.mes,vec.fechaIngreso.anio,vec.legajo);
+    printf("%s\t\t%c\t%.2f\t%d/%d/%d\t\t%d  id:%d\n ",vec.nombre,vec.sexo,vec.sueldo,vec.fechaIngreso.dia,vec.fechaIngreso.mes,vec.fechaIngreso.anio,vec.legajo,vec.idSector);
 }
 void bajaEmpleado(eEmpleado vec[],int tam)
 {
@@ -271,3 +300,57 @@ void modificarEmpleado(eEmpleado vec[], int tam)
 
 }
 
+void hardCode(eSector vec[])
+{
+    vec[0].id=1;
+    strcpy(vec[0].descripcion,"RRHH");
+    vec[0].isEmpty=0;
+
+    vec[1].id=2;
+    strcpy(vec[1].descripcion,"sistema");
+    vec[1].isEmpty=0;
+
+    vec[2].id=3;
+    strcpy(vec[2].descripcion,"administracion");
+    vec[2].isEmpty=0;
+
+    vec[3].id=4;
+    strcpy(vec[3].descripcion,"compras");
+    vec[3].isEmpty=0;
+
+    vec[4].id=5;
+   strcpy(vec[4].descripcion,"deposito");
+    vec[4].isEmpty=0;
+}
+
+/*listar todos los empleados
+
+recorrrer el array de empleado me paro en el elemento 0 con un for ,
+buscp el idSector del empleado
+adentro del for abro otro for y recorro los sectores y comparo el idSector del empleado comparando con el array dee sectores
+ burbuja
+ for(i=0;i<tam;i++)
+ {
+ for(j=0;j<5,j++)
+    {
+    if(empleado[i].idsector==sec[j])
+        {
+        print "%D" empleado[i]
+        %s empleado nombre
+        "%s"sec[j].descripcion;
+        }
+    }
+ }
+*/
+
+/*listar los sectores por empleado
+recorrer los sectores (5)
+con la id del sector recorro el array de empleados y compara , si coincide los muestra
+
+
+for(i=0;i<5;i++)
+    for(j=0j<100;j++)
+    if(sec[i].id==empleado[i].idSec)
+
+    "%d",empleado[j].legajo
+    "%s",empleado[j].nombre
